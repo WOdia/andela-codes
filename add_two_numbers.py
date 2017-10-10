98e49f63b5c9fd586c0d7ed18da4ebe059146292
Wendy Odia <wodia@SamaDc.corp>
2017-10-10 19:38:22 +0300

    added a division function

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -2,3 +2,7 @@ def add_two_numbers(a,b):
   print (a + b)
 
 add_two_numbers(2,5)
+
+def divide_two_numbers(a,b):
+	print (a/b)
+divide_two_numbers(60,5)
---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+print("This is crazy!")
+print("Viola")
+
+def add_two_numbers(a,b):
+  return (a + b)
+
+print "The answer is %d." % (add_two_numbers(5,5))
